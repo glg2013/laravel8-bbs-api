9cee9dd0f8cf3822aab944109a4a2c7aff7fa6d3
glg2013 <57412960@qq.com>
2021-03-01 05:57:27 +0000

    找回密码成功消息提示

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Auth\ForgotPasswordController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\ResetPasswordController;
 use App\Http\Controllers\Auth\VerificationController;
 use App\Http\Controllers\PagesController;
 use Illuminate\Support\Facades\Route;
@@ -36,8 +37,8 @@ Route::post('register', [RegisterController::class, 'register']);
 // 密码重置相关路由
 Route::get('password/reset', [ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');
 Route::post('password/email', [ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
-Route::get('password/reset/{token}', [ForgotPasswordController::class, 'showResetForm'])->name('password.reset');
-Route::post('password/reset', [ForgotPasswordController::class, 'reset'])->name('password.update');
+Route::get('password/reset/{token}', [ResetPasswordController::class, 'showResetForm'])->name('password.reset');
+Route::post('password/reset', [ResetPasswordController::class, 'reset'])->name('password.update');
 
 // Email 认证相关路由
 Route::get('email/verify', [VerificationController::class, 'show'])->name('verification.notice');
