48d07ac30585c700faf99ad7732b92552729a9f3
glg2013 <57412960@qq.com>
2021-03-03 07:50:11 +0000

    回复列表

--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -26,7 +26,7 @@
     </div>
 
     <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12 topic-content">
-      <div class="card ">
+      <div class="card">
         <div class="card-body">
           <h1 class="text-center mt-3 mb-3">
             {{ $topic->title }}
@@ -41,7 +41,6 @@
 
           <div class="topic-body mt-4 mb-4">
             {!! $topic->body !!}
-{{--            {{ $topic->body }}--}}
           </div>
 
           @can('update', $topic)
@@ -64,6 +63,15 @@
 
         </div>
       </div>
+
+      {{-- 用户回复列表 --}}
+      <div class="card topic-reply mt-4">
+        <div class="card-body">
+          @include('topics._reply_box', ['topic' => $topic])
+          @include('topics._reply_list', ['replies' => $topic->replies()->with('user')->get()])
+        </div>
+      </div>
+
     </div>
   </div>
 @stop
