c622f0426f0c7a595f6c523709cb5fcc7f088403
glg2013 <57412960@qq.com>
2021-03-03 10:17:21 +0000

    集成 laravel-permission 扩展包

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -17,12 +17,27 @@ class UsersTableSeeder extends Seeder
         // 生成数据集合
         User::factory()->count(10)->create();
 
-        // 单独处理第一个用户的数据
+        // 单独处理第1个用户的数据
         $user = User::find(1);
         $user->name = 'fengniancong';
         $user->email = 'fengniancong@163.com';
         $user->avatar = 'https://cdn.learnku.com/uploads/images/201710/14/1/ZqM7iaP4CR.png';
         $user->password = bcrypt('glg7850782');
         $user->save();
+
+        // 初始化用户角色，将 1 号用户指派为『站长』
+        $user->assignRole('Founder');
+
+        // 单独处理第2个用户的数据
+        $user = User::find(2);
+        $user->name = 'honglang';
+        $user->email = 'honglang@163.com';
+        $user->avatar = 'https://cdn.learnku.com/uploads/images/201710/14/1/ZqM7iaP4CR.png';
+        $user->password = bcrypt('glg7850782');
+        $user->save();
+
+        // 将 2 号用户指派为『管理员』
+        //$user = User::find(2);
+        $user->assignRole('Maintainer');
     }
 }
