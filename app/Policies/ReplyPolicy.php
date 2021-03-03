16d4472d2fce408e2df077ed4e91eca054687c89
glg2013 <57412960@qq.com>
2021-03-03 09:39:03 +0000

    删除评论

--- app/Policies/ReplyPolicy.php
+++ app/Policies/ReplyPolicy.php
@@ -7,14 +7,9 @@ use App\Models\Reply;
 
 class ReplyPolicy extends Policy
 {
-    public function update(User $user, Reply $reply)
-    {
-        // return $reply->user_id == $user->id;
-        return true;
-    }
-
     public function destroy(User $user, Reply $reply)
     {
-        return true;
+        // 回复的作者 或者 帖子的作者 都可以删除回复
+        return $user->isAuthorOf($reply) || $user->isAuthorOf($reply->topic);
     }
 }
