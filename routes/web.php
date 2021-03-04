86de9240a32635bf2557df27b3306f4305ca16b7
glg2013 <57412960@qq.com>
2021-03-04 06:47:49 +0000

    首页话题列表

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,8 @@ use Illuminate\Support\Facades\Route;
 //    return view('welcome');
 //});
 
-Route::get('/', [PagesController::class, 'root'])->name('root');
+//Route::get('/', [PagesController::class, 'root'])->name('root');
+Route::get('/', [TopicsController::class, 'index'])->name('root');
 
 // 用户身份验证相关的路由
 Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
